af2bea9180c427befe54aa3858b76a9dbda12d4f
nabilmahasin46-hash <nabilmahasin46@gmail.com>
2026-06-02 00:26:06 +0700

    nambah method

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;// => Menentukan port untuk server, menggunakan variabel lingkungan PORT jika tersedia, atau default ke 3000
 app.use(cors({
     origin: 'https://pengingat-obat.pages.dev' // izin untuk akses dari domain ini saja
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'] // metode HTTP yang diizinkan      
+    credentials: true // jika perlu mengirim cookie atau header otentikasi
 }));
 
 // Middleware agar serverm bisa membaca JSON dan melayani file statis
